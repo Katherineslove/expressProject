7c9d384e1bcf76705185e54526e37be9518a8b76
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-08-13 09:51:49 +1200

    updates

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express =  require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(function(req, res, next) {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
-app.get('/', (req, res) => res.send('This is the home page'));
-app.get('/about', (req, res) => res.send('This is the about page'));
-app.get('/contact', (req, res) => res.send('This is the contact page'));
-app.get('/about/me', (req, res) => res.send('This is the me page'));
-app.get('/school', (req, res) => res.send('This is the school page'));
-app.get('/children', (req, res) => res.send('This is the children page'));
+app.use(express.static('./public'))
+
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+// app.get('/', (req, res) => res.send('This is the home page'));
+// app.get('/about', (req, res) => res.send('This is the about page'));
+// app.get('/contact', (req, res) => res.send('This is the contact page'));
+// app.get('/about/me', (req, res) => res.send('This is the me page'));
+// app.get('/school', (req, res) => res.send('This is the school page'));
 // app.post();
-
-
-
-
-
 
 app.listen(port, () => console.log(`application is running on port ${port}`));
